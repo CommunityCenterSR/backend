0a41d46bb00b795a03655aff2c012879774e32ed
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-10-05 12:05:08 -0300

    First commit

---
+++ src/test/java/com/inkua/communitycenter/CommunityCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inkua.communitycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
