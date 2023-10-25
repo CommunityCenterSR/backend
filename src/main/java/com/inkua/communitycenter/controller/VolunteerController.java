9a5f9c6dc77ef809c6e370d906510ad14cf2f4f4
Agustina-Castro <112595749+Agustina-Castro@users.noreply.github.com>
2023-10-25 20:47:19 +0000

    Update VolunteerController.java

--- src/main/java/com/inkua/communitycenter/controller/VolunteerController.java
+++ src/main/java/com/inkua/communitycenter/controller/VolunteerController.java
@@ -2,6 +2,7 @@ package com.inkua.communitycenter.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.inkua.communitycenter.entity.Volunteer;
 import com.inkua.communitycenter.service.VolunteerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/volunteers")
 public class VolunteerController {
 
@@ -32,8 +36,9 @@ public class VolunteerController {
     }
 
     @PostMapping("/")
-    public Volunteer createVolunteer(@RequestBody Volunteer volunteer) {
-        return volunteerService.createVolunteer(volunteer);
+    public ResponseEntity<Volunteer> createVolunteer(@RequestBody Volunteer volunteer) {
+        Volunteer newVolunteer = volunteerService.saveVolunteer(volunteer);
+        return new ResponseEntity<>(newVolunteer, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
@@ -42,7 +47,8 @@ public class VolunteerController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteVolunteer(@PathVariable Long id) {
-        volunteerService.deleteVolunteer(id);
+    public ResponseEntity<Volunteer> deleteVolunteer(@PathVariable Long id) {
+        Volunteer volunteerDeleted = volunteerService.deleteVolunteer(id);
+        return new ResponseEntity<>(volunteerDeleted, HttpStatus.OK);
     }
 }
